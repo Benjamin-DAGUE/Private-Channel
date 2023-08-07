7fd8825407df63617e2905a83c57ba865fadf286
Benjamin-DAGUE <benjamin.dague@grindboxlab.fr>
2023-08-07 23:17:27 +0200

    Note can have embeded password Refactoring code

---
+++ PrivateChannel.Front/Models/AsymetricKey.cs
@@ -0,0 +1,11 @@
+﻿namespace PrivateChannel.Front.Models;
+
+public class AsymetricKey
+{
+    #region Properties
+
+    public List<byte> PublicKey { get; set; } = new List<byte>();
+    public List<byte> PrivateKey { get; set; } = new List<byte>();
+
+    #endregion
+}
---
+++ PrivateChannel.Front/Models/EncryptedMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace PrivateChannel.Front.Models;
+
+public class EncryptedMessage
+{
+    #region Properties
+
+    public List<byte> IV { get; set; } = new List<byte>();
+    public List<byte> EncryptedData { get; set; } = new List<byte>();
+
+    #endregion
+}
